ff293b0689e48faf415bf125fffe868ee926bfba
Mikhail_lesson <mikhail@appscloud.io>
2018-03-10 17:27:33 +0200

    bootstrap lessons 10.03.18

---
+++ assets/javascripts/script.js
@@ -0,0 +1,46 @@
+$( document ).ready(function() {
+
+
+// есть массив с объектами - вывести меню
+    var menuData = [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'About us',
+            href: '/about'
+        },
+        {
+            text: 'Portfolio',
+            href: '#',
+            items: [
+                {
+                    text: 'Websites',
+                    href: '/portfolio/websites'
+                },
+                {
+                    text: 'Design',
+                    href: '/portfolio/design'
+                },
+                {
+                    text: 'SEO',
+                    href: '/portfolio/seo'
+                }
+            ]
+        }
+    ];
+    
+    var wrap = $('#jquery_menu');
+    $(wrap).attr('id', 'navbarNavDropdown2');
+    $(wrap).css('display', 'block');
+    $(wrap).addClass('collapse');
+    $(wrap).addClass('navbar-collapse');
+    $(wrap).append("<nav></nav>");
+    var navbar = $(wrap).find('nav');
+    for (var i = 0; i < menuData.length; i++) {        
+        var menuText = $("<li></li>").html(menuData[i].text); 
+        $(navbar).append(menuText);
+    }
+
+});
